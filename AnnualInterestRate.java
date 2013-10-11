18feb28a8d7ea1eceb30da6a0737a2e119202eb3
Matt Knicos <mknicos@gmail.com>
2013-10-11 09:28:13 -0500

    Added annual interst rate program

---
+++ AnnualInterestRate.java
@@ -0,0 +1,20 @@
+package chapterTwoExercizes;
+
+import acm.program.*;
+@SuppressWarnings("serial")
+
+/*
+ * This program will compute a final account balance based on an initial balance and
+ * an annual interest rate
+ */
+
+
+public class AnnualInterestRate extends ConsoleProgram{
+	public void run(){
+		println("This program computes an account balance based on annual interest rate.");
+		double balance = readDouble("Enter begining account balance: ");
+		double rate = readDouble("Enter interest rate as a percentage: ");
+		double endBalance = (balance * (rate/100)) + balance;
+		println("The final balnce after one year is " + endBalance);
+	}
+}

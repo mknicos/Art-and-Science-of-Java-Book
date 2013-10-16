61f57e22f6ce49080b154ef744e048d3c195bf06
Matt Knicos <mknicos@gmail.com>
2013-10-16 14:41:14 -0500

    BottlesOfBeer program now accepts user input, instead of constant variable

--- BottlesOfBeer.java
+++ BottlesOfBeer.java
@@ -5,8 +5,9 @@
  * This program is based on The Art and Science of Java by Eric S. Roberts
  * Chapter 4, Exercise 1
  * It will print to the screen the 99 bottles of beer song, in descending integers
- * Since 99 is a lot of verses to write out, I will make the Starting number of
- * "beers on the wall" variable, using a Constant Variable
+ * However, 99 verses is a lot to write out. In the first version of this program
+ * the number of verses was varied by a constant variable, however, in this version
+ * the program will ask for users input as for where to start the song.
  */
 
 
@@ -16,7 +17,9 @@ import acm.program.*;
 public class BottlesOfBeer extends ConsoleProgram {
 	public void run(){
 		println("Lets sing a song together..");
-		int startNum = INIT_NUM;
+		println("We are going to sing the ever popular hit, 99 bottles of beer");
+		println("But 99 takes such a long time :-(");
+		int startNum = readInt("What number would you like the song to start at? ");
 		while(startNum > 0){
 		println(startNum + " bottles of beer on the wall,");
 		println(startNum + " bottles of beer");
@@ -24,8 +27,5 @@ public class BottlesOfBeer extends ConsoleProgram {
 		startNum --;
 		println(startNum + " bottles of beer on the wall");
 		}
-		
 	}
-	
-	private static final int INIT_NUM = 6;
 }
